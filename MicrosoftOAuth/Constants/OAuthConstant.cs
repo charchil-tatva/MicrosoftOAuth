5c1a5e533f9c849d62c5a0f4732efb3a7b1fa0a9
charchil-tatva <charchil.kajaliya@tatvamail.com>
2023-10-04 17:19:13 +0530

    Add project files.

---
+++ MicrosoftOAuth/Constants/OAuthConstant.cs
@@ -0,0 +1,11 @@
+﻿namespace MicrosoftOAuth.Constants;
+
+public static class OAuthConstant
+{
+    public const string CLIENT_ID = "c953b1eb-bdf8-4bd2-8a88-a1b992e52003";
+    public const string CLIENT_SECRET = "F7x8Q~26yPV1nNAF1m-LnkaKsyjqw7jEp8~YYdc0";
+    public const string REDIRECT_URL = "https://localhost:7088/api/OAuth/callback";
+    public const string SCOPES = "openid offline_access";
+    public const string CODE_VERIFIER = "7e2a6b7d-8c65-4d16-b437-6c2870a8199e";
+    public const string CODE_CHALLENGE = "cm5e7eCH-3mv1po9pTeZ_OWELysEIVsXOQqy1s-IyME";
+}
---
+++ MicrosoftOAuth/Controllers/OAuthController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MicrosoftOAuth.Constants;
+using RestSharp;
+
+namespace MicrosoftOAuth.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class OAuthController : ControllerBase
+{
+    [HttpGet("getauthurl")]
+    public ActionResult GetAuthUrl()
+    {
+        return Ok(new {
+            url = $"https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=${OAuthConstant.CLIENT_ID}&response_type=code&redirect_uri=${OAuthConstant.REDIRECT_URL}&response_mode=query&scope=openid%20offline_access&state=12345"
+        }) ;
+    }
+
+
+    [HttpGet("callback")]
+    public async Task<ActionResult> Callback(string code, string state, string session_state)
+    {
+        if (!String.IsNullOrWhiteSpace(code))
+        {
+            RestClient restClient = new RestClient();
+            RestRequest restRequest = new RestRequest("https://login.microsoftonline.com/common/oauth2/v2.0/token", Method.Post);
+
+            restRequest.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+
+            restRequest.AddParameter("code", code);
+            restRequest.AddParameter("grant_type", "authorization_code");
+            restRequest.AddParameter("client_id", OAuthConstant.CLIENT_ID);
+            restRequest.AddParameter("client_secret", OAuthConstant.CLIENT_SECRET);
+            restRequest.AddParameter("scope", OAuthConstant.SCOPES);
+            restRequest.AddParameter("redirect_uri", OAuthConstant.REDIRECT_URL);
+            //restRequest.AddParameter("code_verifier", OAuthConstant.CODE_VERIFIER); // Include the code verifier
+            //restRequest.AddParameter("code_challenge", OAuthConstant.CODE_CHALLENGE); // Include the code challenge
+            //restRequest.AddParameter("code_challenge_method", "S256");
+
+            RestResponse response = await restClient.ExecuteAsync(restRequest);
+
+            Console.WriteLine(response.Content);
+
+            return Redirect("http://localhost:3000");
+        }
+        else
+        {
+            return BadRequest("bad request");
+        }
+    }
+}
